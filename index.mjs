8f46d88b2f60df2eb9e9b25fbcf3c69fda76a67e
AuthorOf <authorofdisclose@gmail.com>
2024-02-01 13:19:38 +0600

    third commit

--- index.mjs
+++ index.mjs
@@ -3,8 +3,22 @@ const server = http.createServer((req, res) => {
     if(req.url === '/'){
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(`
-        <h1 styles='font-family: verdana'>WELCOME TO MY WEB APP</h1>
-        <p styles='font-family: verdana'>But it is under construction now. Sorry</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Webpage</title>
+</head>
+<style>
+*{
+    font-family: 'Geist Mono';
+}
+</style>
+<body>
+    <h1>Welcome to my Web APP</h1>
+</body>
+</html>
         `);
     }else{
         res.writeHead(200, {'Content-Type': 'text/html'});
